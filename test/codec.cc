3879ea7093e8b5c8ddd57cf34f79e5bd7bb46268
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-06-25 12:40:58 -0700

    Fixed narrowing conversions for codec unittests

--- test/codec.cc
+++ test/codec.cc
@@ -8,8 +8,6 @@
 #include "byteorder.h"
 #include "codec.h"
 #include <gtest/gtest.h>
-#include <warnings/push.h>
-#include <warnings/narrowing-conversions.h>
 
 // CONSTANTS
 // ---------
@@ -17,11 +15,11 @@
 // 한국어
 static const std::string UTF8 = {-19, -107, -100, -22, -75, -83, -20, -106, -76};
 #if BYTE_ORDER == LITTLE_ENDIAN
-static const std::u16string UTF16 = {-10916, -21139, -14924};
+static const std::u16string UTF16 = {54620, 44397, 50612};
 static const std::u32string UTF32 = {54620, 44397, 50612};
 #else
-static const std::u16string UTF16 = {23765,  28077, -19259};
-static const std::u32string UTF32 = {1557463040,  1840054272, -1262157824};
+static const std::u16string UTF16 = {23765, 28077, 46277};
+static const std::u32string UTF32 = {1557463040, 1840054272, 3032809472};
 #endif
 
 // räksmörgås
@@ -30,8 +28,8 @@ static const std::string UTF8_2 = {114, -61, -92, 107, 115, 109, -61, -74, 114, 
 static const std::u16string UTF16_2 = {114,  228,  107,  115,  109,  246,  114,  103,  229,  115};
 static const std::u32string UTF32_2 = {114, 228, 107, 115, 109, 246, 114, 103, 229, 115};
 #else
-static const std::u16string UTF16_2 = {29184, -7168, 27392, 29440, 27904, -2560, 29184, 26368, -6912, 29440};
-static const std::u32string UTF32_2 = {1912602624, -469762048, 1795162112, 1929379840, 1828716544, -167772160, 1912602624, 1728053248, -452984832, 1929379840};
+static const std::u16string UTF16_2 = {29184, 58368, 27392, 29440, 27904, 62976, 29184, 26368, 58624, 29440};
+static const std::u32string UTF32_2 = {1912602624, 3825205248, 1795162112, 1929379840, 1828716544, 4127195136, 1912602624, 1728053248, 3841982464, 1929379840};
 #endif
 
 // "Même"
@@ -40,8 +38,8 @@ static const std::string UTF8_3 = {77, -61, -86, 109, 101};
 static const std::u16string UTF16_3 = {77, 234, 109, 101};
 static const std::u32string UTF32_3 = {77, 234, 109, 101};
 #else
-static const std::u16string UTF16_3 = {19712, -5632, 27904, 25856};
-static const std::u32string UTF32_3 = {1291845632, -369098752, 1828716544, 1694498816};
+static const std::u16string UTF16_3 = {19712, 59904, 27904, 25856};
+static const std::u32string UTF32_3 = {1291845632, 3925868544, 1828716544, 1694498816};
 #endif
 
 // TESTS
@@ -74,5 +72,3 @@ TEST(codec, codepoint_conversions)
     EXPECT_EQ(codec_utf32_utf16(UTF32_2), UTF16_2);
     EXPECT_EQ(codec_utf32_utf16(UTF32_3), UTF16_3);
 }
-
-#include <warnings/pop.h>
