e8cd58787c5ca5a37abf03a7f930230c0e7c776d
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-07-07 20:25:29 -0700

    Finished most of the XML interface

--- src/xml/core.cc
+++ src/xml/core.cc
@@ -2,45 +2,442 @@
 //  :license: MIT, see licenses/mit.md for more details.
 
 #include <xml/core.h>
+#include <multi_index_container.hpp>
+#include <multi_index/hashed_index.hpp>
+#include <multi_index/indexed_by.hpp>
+#include <multi_index/mem_fun.hpp>
+#include <multi_index/sequenced_index.hpp>
+#include <atomic>
 #include <stdexcept>
+
+// ALIAS
+// -----
+
+struct name_tag {};
+struct id_tag {};
+
+
+typedef multi_index_container<
+    xml_node_t,
+    multi_index::indexed_by<
+        multi_index::sequenced<>,
+        multi_index::hashed_non_unique<
+            multi_index::tag<name_tag>,
+            multi_index::const_mem_fun<xml_node_t, const xml_string_t&, &xml_node_t::get_tag>,
+            std::hash<xml_string_t>
+        >,
+        multi_index::hashed_unique<
+            multi_index::tag<id_tag>,
+            multi_index::const_mem_fun<xml_node_t, const xml_id_t&, &xml_node_t::get_id>,
+            std::hash<xml_id_t>
+        >
+    >
+> xml_node_list_impl_t;
+
+using xml_node_iterator_impl_t = typename xml_node_list_impl_t::iterator;
+
+// HELPERS
+// -------
+
+
+/**
+ *  \brief Get an effectively unique identifier for the document.
+ *
+ *  This is a temporary identifier while the document is in memory,
+ *  and is not stable. Therefore, 4 bytes of precision should suffice.
+ */
+static xml_id_t get_unique_id()
+{
+    static std::atomic<xml_id_t> seed = ATOMIC_VAR_INIT(0);
+    return seed++;
+}
+
+
+// PRIVATE
+// -------
+
+
+/**
+ *  \brief Private implementation for an xml_node_t.
+ *
+ *  Hide away the interface to allow copying individual items
+ *  to preserve the same identifier, and be essentially equivalent
+ *  to copying a shared_ptr.
+ */
+struct xml_node_impl_t
+{
+    xml_string_t tag;
+    xml_string_t text;
+    xml_attr_t attrs;
+    xml_node_list_t children;
+    xml_id_t id = get_unique_id();
+    xml_node_list_t* parent = nullptr;
+};
+
 
 // OBJECTS
 // -------
 
 
+xml_node_iterator_t::xml_node_iterator_t(const self& other):
+    ptr_(other.ptr_)
+{}
+
+
+auto xml_node_iterator_t::operator=(const self& other) -> xml_node_iterator_t&
+{
+    ptr_ = other.ptr_;
+    return *this;
+}
+
+
+xml_node_iterator_t::xml_node_iterator_t(self&& other)
+{
+    swap(other);
+}
+
+
+auto xml_node_iterator_t::operator=(self&& other) -> xml_node_iterator_t&
+{
+    swap(other);
+    return *this;
+}
+
+
+bool xml_node_iterator_t::operator==(const self& other) const
+{
+    auto* l = (const xml_node_iterator_impl_t*) ptr_;
+    auto* r = (const xml_node_iterator_impl_t*) other.ptr_;
+    return *l == *r;
+}
+
+
+bool xml_node_iterator_t::operator!=(const self& other) const
+{
+    return !operator==(other);
+}
+
+
+auto xml_node_iterator_t::operator++() -> self&
+{
+    auto& it = *(xml_node_iterator_impl_t*) ptr_;
+    ++it;
+    return *this;
+}
+
+
+auto xml_node_iterator_t::operator++(int) -> self
+{
+    self copy(*this);
+    operator++();
+    return copy;
+}
+
+
+auto xml_node_iterator_t::operator--() -> self&
+{
+    auto& it = *(xml_node_iterator_impl_t*) ptr_;
+    --it;
+    return *this;
+}
+
+
+auto xml_node_iterator_t::operator--(int) -> self
+{
+    self copy(*this);
+    operator--();
+    return copy;
+}
+
+
+auto xml_node_iterator_t::operator*() -> reference
+{
+    auto& it = *(xml_node_iterator_impl_t*) ptr_;
+    return const_cast<reference>(*it);
+}
+
+
+auto xml_node_iterator_t::operator*() const -> const_reference
+{
+    auto& it = *(xml_node_iterator_impl_t*) ptr_;
+    return *it;
+}
+
+
+auto xml_node_iterator_t::operator->() -> pointer
+{
+    auto& it = *(xml_node_iterator_impl_t*) ptr_;
+    return const_cast<pointer>(&*it);
+}
+
+
+auto xml_node_iterator_t::operator->() const -> const_pointer
+{
+    auto& it = *(xml_node_iterator_impl_t*) ptr_;
+    return &*it;
+}
+
+
+void xml_node_iterator_t::swap(self& other)
+{
+    std::swap(ptr_, other.ptr_);
+}
+
+
+xml_node_list_t::xml_node_list_t():
+    ptr_(new xml_node_list_impl_t)
+{}
+
+
+xml_node_list_t::~xml_node_list_t()
+{
+    delete (xml_node_list_impl_t*) ptr_;
+}
+
+
+auto xml_node_list_t::begin() -> iterator
+{}
+
+
+auto xml_node_list_t::begin() const -> const_iterator
+{}
+
+
+auto xml_node_list_t::cbegin() const -> const_iterator
+{}
+
+
+auto xml_node_list_t::end() -> iterator
+{}
+
+
+auto xml_node_list_t::end() const -> const_iterator
+{}
+
+
+auto xml_node_list_t::cend() const -> const_iterator
+{}
+
+
+auto xml_node_list_t::rbegin() -> reverse_iterator
+{}
+
+
+auto xml_node_list_t::rbegin() const -> const_reverse_iterator
+{}
+
+
+auto xml_node_list_t::crbegin() const -> const_reverse_iterator
+{}
+
+
+auto xml_node_list_t::rend() -> reverse_iterator
+{}
+
+
+auto xml_node_list_t::rend() const -> const_reverse_iterator
+{}
+
+
+auto xml_node_list_t::crend() const -> const_reverse_iterator
+{
+    // TODO: implement...
+}
+
+
+bool xml_node_list_t::operator==(const self& other) const
+{
+    auto* l = (const xml_node_list_impl_t*) ptr_;
+    auto* r = (const xml_node_list_impl_t*) other.ptr_;
+    return *l == *r;
+}
+
+
+bool xml_node_list_t::operator!=(const self& other) const
+{
+    return !operator==(other);
+}
+
+
+void xml_node_list_t::swap(self& other)
+{
+    std::swap(ptr_, other.ptr_);
+}
+
+
 xml_node_t::xml_node_t():
-    parent_(nullptr)
+    ptr_(new xml_node_impl_t)
 {}
 
 
 xml_node_t::xml_node_t(xml_node_list_t& parent):
-    parent_(&parent)
-{}
+    ptr_(new xml_node_impl_t)
+{
+    ptr_->parent = &parent;
+}
+
+
+auto xml_node_t::begin() -> iterator
+{
+    return get_children().begin();
+}
+
+
+auto xml_node_t::begin() const -> const_iterator
+{
+    return get_children().begin();
+}
+
+
+auto xml_node_t::cbegin() const -> const_iterator
+{
+    return get_children().cbegin();
+}
+
+
+auto xml_node_t::end() -> iterator
+{
+    return get_children().end();
+}
+
+
+auto xml_node_t::end() const -> const_iterator
+{
+    return get_children().end();
+}
+
+
+auto xml_node_t::cend() const -> const_iterator
+{
+    return get_children().cend();
+}
+
+
+auto xml_node_t::rbegin() -> reverse_iterator
+{
+    return get_children().rbegin();
+}
+
+
+auto xml_node_t::rbegin() const -> const_reverse_iterator
+{
+    return get_children().rbegin();
+}
+
+
+auto xml_node_t::crbegin() const -> const_reverse_iterator
+{
+    return get_children().crbegin();
+}
+
+
+auto xml_node_t::rend() -> reverse_iterator
+{
+    return get_children().rend();
+}
+
+
+auto xml_node_t::rend() const -> const_reverse_iterator
+{
+    return get_children().rend();
+}
+
+
+auto xml_node_t::crend() const -> const_reverse_iterator
+{
+    return get_children().crend();
+}
 
 
 const xml_string_t& xml_node_t::get_tag() const
 {
-    return tag_;
+    return ptr_->tag;
 }
 
 
 const xml_string_t& xml_node_t::get_text() const
 {
-    return text_;
+    return ptr_->text;
+}
+
+
+xml_attr_t& xml_node_t::get_attrs()
+{
+    return ptr_->attrs;
+}
+
+
+const xml_attr_t& xml_node_t::get_attrs() const
+{
+    return ptr_->attrs;
+}
+
+
+xml_node_list_t& xml_node_t::get_children()
+{
+    return ptr_->children;
+}
+
+
+const xml_node_list_t& xml_node_t::get_children() const
+{
+    return ptr_->children;
+}
+
+
+const xml_id_t& xml_node_t::get_id() const
+{
+    return ptr_->id;
 }
 
 
 void xml_node_t::set_tag(const xml_string_t& tag)
 {
-    if (parent_) {
-        // TODO: need to signify to the parent the element has been modified.
+    if (ptr_->parent) {
+        // get an iterator to our element in constant time
+        auto &container = *(xml_node_list_impl_t*) ptr_->parent->ptr_;
+        auto &view = container.get<id_tag>();
+        auto it = view.find(ptr_->id);
+        if (it == view.end()) {
+            throw std::runtime_error("Node has no visible parent.");
+        }
+
+        // update the underlying container
+        view.modify(it, [&tag](xml_node_t &node) {
+            node.ptr_->tag = tag;
+        });
+
     } else {
-        tag_ = tag;
+        ptr_->tag = tag;
     }
 }
 
 
 void xml_node_t::set_text(const xml_string_t& text)
 {
-    text_ = text;
+    ptr_->text = text;
+}
+
+
+void xml_node_t::set_attrs(const xml_attr_t& attrs)
+{
+    ptr_->attrs = attrs;
+}
+
+
+void xml_node_t::set_children(const xml_node_list_t& children)
+{
+    ptr_->children = children;
+}
+
+
+bool xml_node_t::operator==(const self& other) const
+{
+    return get_id() == other.get_id();
+}
+
+
+bool xml_node_t::operator!=(const self& other) const
+{
+    return !operator==(other);
 }
--- src/xml/core.h
+++ src/xml/core.h
@@ -8,6 +8,8 @@
 #pragma once
 
 #include <cstdint>
+#include <iterator>
+#include <memory>
 #include <string>
 #include <unordered_map>
 
@@ -34,11 +36,14 @@
 // -------
 
 struct xml_node_t;
+struct xml_node_impl_t;
 struct xml_node_list_t;
+struct xml_node_iterator_t;
 
 // ALIAS
 // -----
 
+typedef uint32_t xml_id_t;
 typedef std::string xml_string_t;
 typedef std::unordered_map<xml_string_t, xml_string_t> xml_attr_t;
 
@@ -49,15 +54,99 @@ typedef std::unordered_map<xml_string_t, xml_string_t> xml_attr_t;
 // TODO: need an iterator wrapper too...
 
 /**
+ *  \brief Iterator over XML nodes.
+ */
+struct xml_node_iterator_t: std::iterator<std::bidirectional_iterator_tag, xml_node_t>
+{
+public:
+    // MEMBER TYPES
+    // ------------
+    typedef xml_node_iterator_t self;
+    typedef std::iterator<std::bidirectional_iterator_tag, xml_node_t> base;
+    using typename base::value_type;
+    using typename base::difference_type;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+
+    // CONSTRUCTORS
+    xml_node_iterator_t() = delete;
+    xml_node_iterator_t(const self&);
+    self & operator=(const self&);
+    xml_node_iterator_t(self&&);
+    self & operator=(self&&);
+    ~xml_node_iterator_t();
+
+    // RELATIONAL OPERATORS
+    bool operator==(const self&) const;
+    bool operator!=(const self&) const;
+
+    // INCREMENTORS
+    self& operator++();
+    self operator++(int);
+    self& operator--();
+    self operator--(int);
+
+    // DEREFERENCE
+    reference operator*();
+    const_reference operator*() const;
+    pointer operator->();
+    const_pointer operator->() const;
+
+    // OTHER
+    void swap(self&);
+
+private:
+    void *ptr_ = nullptr;
+};
+
+
+/**
  *  \brief Collection of ordered elements with name-based lookups.
  */
 struct xml_node_list_t
 {
 public:
+    // MEMBER TYPES
+    // ------------
+    typedef xml_node_list_t self;
+    typedef xml_node_iterator_t iterator;
+    typedef iterator const_iterator;
+    typedef std::reverse_iterator<iterator> reverse_iterator;
+    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+    // MEMBER FUNCTIONS
+    // ----------------
     xml_node_list_t();
+    ~xml_node_list_t();
+
+    // ITERATORS
+    iterator begin();
+    const_iterator begin() const;
+    const_iterator cbegin() const;
+    iterator end();
+    const_iterator end() const;
+    const_iterator cend() const;
+    reverse_iterator rbegin();
+    const_reverse_iterator rbegin() const;
+    const_reverse_iterator crbegin() const;
+    reverse_iterator rend();
+    const_reverse_iterator rend() const;
+    const_reverse_iterator crend() const;
+
+    // RELATIONAL OPERATORS
+    bool operator==(const self&) const;
+    bool operator!=(const self&) const;
+
+    // MODIFIERS
+//    void clear();
+    void swap(self&);
 
     // TODO:
 private:
+    friend class xml_node_t;
+
     void *ptr_ = nullptr;
 };
 
@@ -70,27 +159,60 @@ private:
 struct xml_node_t
 {
 public:
+    // MEMBER TYPES
+    // ------------
+    typedef xml_node_t self;
+    typedef xml_node_iterator_t iterator;
+    typedef iterator const_iterator;
+    typedef std::reverse_iterator<iterator> reverse_iterator;
+    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+    // MEMBER FUNCTIONS
+    // ----------------
+
+    // CONSTRUCTORS
     xml_node_t();
     xml_node_t(xml_node_list_t&);
-    // TODO: document the interface
+    xml_node_t(const xml_node_t&) = default;
+    xml_node_t & operator=(const xml_node_t&) = default;
+    xml_node_t(xml_node_t&&) = default;
+    xml_node_t & operator=(xml_node_t&&) = default;
 
-    // TODO:....
-    void clear();
+    // ITERATORS
+    iterator begin();
+    const_iterator begin() const;
+    const_iterator cbegin() const;
+    iterator end();
+    const_iterator end() const;
+    const_iterator cend() const;
+    reverse_iterator rbegin();
+    const_reverse_iterator rbegin() const;
+    const_reverse_iterator crbegin() const;
+    reverse_iterator rend();
+    const_reverse_iterator rend() const;
+    const_reverse_iterator crend() const;
 
     // GETTERS
     const xml_string_t& get_tag() const;
     const xml_string_t& get_text() const;
-// TODO: this doesn't need to be const
-//    const xml_attr_t& get_attrs() const;
+    xml_attr_t& get_attrs();
+    const xml_attr_t& get_attrs() const;
+    xml_node_list_t& get_children();
+    const xml_node_list_t& get_children() const;
+    const xml_id_t& get_id() const;
 
     // SETTERS
     void set_tag(const xml_string_t&);
     void set_text(const xml_string_t&);
+    void set_attrs(const xml_attr_t&);
+    void set_children(const xml_node_list_t&);
+
+    // RELATIONAL OPERATORS
+    bool operator==(const self&) const;
+    bool operator!=(const self&) const;
 
 private:
-    xml_string_t tag_;
-    xml_string_t text_;
-    xml_attr_t attr_;
-    xml_node_list_t children_;
-    xml_node_list_t* parent_ = nullptr;
+    friend class xml_node_list_t;
+
+    std::shared_ptr<xml_node_impl_t> ptr_;
 };
