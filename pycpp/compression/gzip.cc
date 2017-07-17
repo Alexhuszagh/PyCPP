6a57b84a9b39ee3947120ef132b0776cf35c45b5
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-07-17 03:19:24 -0700

    Wrote the gzip compressor

--- pycpp/compression/gzip.cc
+++ pycpp/compression/gzip.cc
@@ -1,5 +1,7 @@
+//  :copyright: (c) 2003-2007 Jonathan Turkanis.
+//  :copyright: (c) 2008 CodeRage, LLC (turkanis at coderage dot com).
 //  :copyright: (c) 2017 Alex Huszagh.
-//  :license: MIT, see licenses/mit.md for more details.
+//  :license: Boost, see licenses/boost.md for more details.
 
 // This module is basically identical to zlib,
 // so just include the private error handling.
@@ -7,17 +9,65 @@
 #include <pycpp/compression/zlib.cc>
 #include <pycpp/compression/gzip.h>
 #include <cstring>
+#include <ctime>
 
 PYCPP_BEGIN_NAMESPACE
-
-// TODO: there's a lot wrong with this module, need to fix it
-// https://github.com/boostorg/iostreams/blob/develop/include/boost/iostreams/filter/gzip.hpp
 
 // MACROS
 // ------
 
 #define WINDOW_BITS 15
 #define GZIP_ENCODING 16
+
+// HELPERS
+// -------
+
+static std::string gzip_header(int level,
+    std::time_t mtime = 0,
+    const std::string& filename = "",
+    const std::string& comment = "")
+{
+    std::string header;
+    bool has_filename = !filename.empty();
+    bool has_comment = !comment.empty();
+    size_t length = 10;
+    if (has_filename) {
+        length += filename.size() + 1;
+    }
+    if (has_comment) {
+        length += comment.size() + 1;
+    }
+    header.reserve(length);
+
+    header += (char) 0x1f;                  /* magic number */
+    header += (char) 0x8b;                  /* magic number */
+    header += (char) 0x08;                  /*    deflate   */
+    header += (char) 0x00;                  /*     flags    */
+
+    // mtime
+    uint32_t mtime_le = htole32(mtime);
+    header.append((char*) &mtime_le, sizeof(uint32_t));
+
+    if (level == Z_BEST_COMPRESSION) {
+        header += (char) 0x02;              /*  extra flags */
+    } else if (level == Z_BEST_SPEED) {
+        header += (char) 0x04;              /*  extra flags */
+    } else {
+        header += (char) 0x00;              /*  extra flags */
+    }
+    header += (char) 0xff;                  /*  OS unknown  */
+
+    if (has_filename) {
+        header += filename;
+        header += (char) 0x00;
+    }
+    if (has_comment) {
+        header += comment;
+        header += (char) 0x00;
+    }
+
+    return header;
+}
 
 // OBJECTS
 // -------
@@ -34,6 +84,8 @@ struct gzip_compressor_impl: filter_impl<z_stream>
     gzip_compressor_impl(int level = 9);
     ~gzip_compressor_impl();
 
+    void write_header();
+    void write_footer(void*& dst);
     virtual void call();
     bool flush(void*& dst, size_t dstlen);
     compression_status operator()(const void*& src, size_t srclen, void*& dst, size_t dstlen);
@@ -42,11 +94,15 @@ struct gzip_compressor_impl: filter_impl<z_stream>
 
 gzip_compressor_impl::gzip_compressor_impl(int level)
 {
+    header = gzip_header(level);
+
     status = Z_OK;
     stream.zalloc = Z_NULL;
     stream.zfree = Z_NULL;
     stream.opaque = Z_NULL;
-    CHECK(deflateInit2(&stream, level, Z_DEFLATED, WINDOW_BITS | GZIP_ENCODING, 8, Z_DEFAULT_STRATEGY));
+    CHECK(deflateInit2(&stream, level, Z_DEFLATED, -WINDOW_BITS, 8, Z_DEFAULT_STRATEGY));
+    // TODO: inflateInit2 will use -WINDOW_BITS
+    // inflateInit2(s, window_bits)
 }
 
 
@@ -56,12 +112,45 @@ gzip_compressor_impl::~gzip_compressor_impl()
 }
 
 
+void gzip_compressor_impl::write_header()
+{
+    // write header on first pass
+    if (!header.empty() && header.size() < stream.avail_out) {
+        memcpy(stream.next_out, header.data(), header.size());
+        stream.next_out += header.size();
+        stream.avail_out -= header.size();
+        header.clear();
+    }
+}
+
+
+void gzip_compressor_impl::write_footer(void*& dst)
+{
+    if (status == Z_STREAM_END && stream.avail_out >= 8) {
+        // write CRC32
+        uint32_t crc_le = htole32(crc);
+        memcpy(stream.next_out, &crc_le, sizeof(uint32_t));
+        stream.next_out += sizeof(uint32_t);
+
+        // write size
+        uint32_t size_le = htole32(size & 0xffffffff);
+        memcpy(stream.next_out, &size_le, sizeof(uint32_t));
+        stream.next_out += sizeof(uint32_t);
+        after(dst);
+    }
+}
+
+
 void gzip_compressor_impl::call()
 {
+    // try to write header, and if cannot, return early
+    write_header();
+    if (!header.empty()) {
+        return;
+    }
+
     size += stream.avail_in;
     crc = crc32(crc, stream.next_in, stream.avail_in);
-    printf("New CRC is %u\n", crc);
-    printf("New size is %zu\n", size);
     while (stream.avail_in && stream.avail_out && status != Z_STREAM_END) {
         status = deflate(&stream, Z_NO_FLUSH);
         check_zstatus(status);
@@ -71,6 +160,12 @@ void gzip_compressor_impl::call()
 
 bool gzip_compressor_impl::flush(void*& dst, size_t dstlen)
 {
+    // try to write header, and if cannot, return early
+    write_header();
+    if (!header.empty()) {
+        return false;
+    }
+
     bool code = base::flush(dst, dstlen, [&]()
     {
         if (dstlen) {
@@ -81,20 +176,7 @@ bool gzip_compressor_impl::flush(void*& dst, size_t dstlen)
             return status == Z_STREAM_END || status == Z_OK;
         }
     });
-
-    if (code && stream.avail_out >= 8) {
-        char* buf = (char*) dst;
-        // write CRC32
-        uint32_t crc_le = htole32(crc);
-        memcpy(buf, &crc_le, sizeof(uint32_t));
-        buf += sizeof(uint32_t);
-
-        // write size
-        uint32_t size_le = htole32(size & 0xffffffff);
-        memcpy(buf, &size_le, sizeof(uint32_t));
-        buf += sizeof(uint32_t);
-        dst = buf;
-    }
+    write_footer(dst);
 
     return code;
 }
