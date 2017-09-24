bc2701e43d49f1f1578faf656ef9c5b253b01fff
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-09-24 12:33:34 -0700

    Added whitespace module

---
+++ pycpp/string/whitespace.h
@@ -0,0 +1,24 @@
+//  :copyright: (c) 2017 Alex Huszagh.
+//  :license: MIT, see licenses/mit.md for more details.
+/**
+ *  \addtogroup PyCPP
+ *  \brief Cross-platform whitespace defintions.
+ */
+
+#pragma once
+
+#include <pycpp/config.h>
+#include <pycpp/preprocessor/os.h>
+
+PYCPP_BEGIN_NAMESPACE
+
+// CONSTANTS
+// ---------
+
+#if OS_WINDOWS
+    static constexpr char NEWLINE[] = "\r\n";
+#else
+    static constexpr char NEWLINE[] = "\n";
+#endif
+
+PYCPP_END_NAMESPACE
