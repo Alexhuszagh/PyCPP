58503f1ba069f656e2a0f55ad8ebab5baa8df435
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-12-06 21:49:15 -0800

    Bug fix for PIMPL to fix a lot of undefined behavior

--- pycpp/misc/pimpl.h
+++ pycpp/misc/pimpl.h
@@ -17,7 +17,6 @@ PYCPP_BEGIN_NAMESPACE
 // DECLARATION
 // -----------
 
-
 /**
  *  \brief pimpl idiom using aligned storage for direct stack allocation.
  *
@@ -74,57 +73,61 @@ pimpl<T, S>::pimpl()
 
 
 template <typename T, size_t S>
-pimpl<T, S>::pimpl(const self& other):
-    mem(other.mem)
-{}
+pimpl<T, S>::pimpl(const self& other)
+{
+    new(reinterpret_cast<pointer>(&mem)) value_type(*other);
+}
 
 
 template <typename T, size_t S>
 auto pimpl<T, S>::operator=(const self& other) -> self&
 {
-    mem = other.mem;
+    new(reinterpret_cast<pointer>(&mem)) value_type(*other);
     return *this;
 }
 
 
 template <typename T, size_t S>
-pimpl<T, S>::pimpl(pimpl&& other):
-    mem(std::move(other.mem))
-{}
+pimpl<T, S>::pimpl(pimpl&& other)
+{
+    new(reinterpret_cast<pointer>(&mem)) value_type(std::move(*other));
+}
 
 
 template <typename T, size_t S>
 auto pimpl<T, S>::operator=(self&& other) -> self&
 {
-    mem = std::move(other.mem);
+    new(reinterpret_cast<pointer>(&mem)) value_type(std::move(*other));
     return *this;
 }
 
 
 template <typename T, size_t S>
-pimpl<T, S>::pimpl(const value_type& other):
-    mem(reinterpret_cast<const memory_type&>(other))
-{}
+pimpl<T, S>::pimpl(const value_type& other)
+{
+    new(reinterpret_cast<pointer>(&mem)) value_type(other);
+}
 
 
 template <typename T, size_t S>
 auto pimpl<T, S>::operator=(const value_type& other) -> self&
 {
-    **this = other;
+    new(reinterpret_cast<pointer>(&mem)) value_type(other);
     return *this;
 }
 
 
 template <typename T, size_t S>
-pimpl<T, S>::pimpl(value_type&& other):
-    mem(std::move(reinterpret_cast<memory_type&&>(other)))
-{}
+pimpl<T, S>::pimpl(value_type&& other)
+{
+    new(reinterpret_cast<pointer>(&mem)) value_type(std::move(other));
+}
 
 
 template <typename T, size_t S>
 auto pimpl<T, S>::operator=(value_type&& other) -> self&
 {
-    **this = std::move(other);
+    new(reinterpret_cast<pointer>(&mem)) value_type(std::move(other));
     return *this;
 }
 
